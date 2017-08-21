1e2b75eadf845e3d76df5e1de37e87de30d00d66
matheisson <csanyil@gmail.com>
2017-08-21 17:38:26 +0200

    unit model and route added

--- app/Website-App/App_Stock/admin.py
+++ app/Website-App/App_Stock/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from App_Stock.models import Firm, Commodity
+from App_Stock.models import Firm, Commodity, Units, CommodityUnitRelation
 
 
 class FirmAdmin(admin.ModelAdmin):
@@ -11,5 +11,19 @@ class CommodityAdmin(admin.ModelAdmin):
 
     search_fields = ('name', 'price')
 
+
+class UnitsAdmin(admin.ModelAdmin):
+
+    search_field = 'name'
+
+
+class CommodityUnitAdmin(admin.ModelAdmin):
+
+    search_fields = ('unit', 'commodity')
+
+
 admin.site.register(Firm, FirmAdmin)
 admin.site.register(Commodity, CommodityAdmin)
+admin.site.register(Units, UnitsAdmin)
+admin.site.register(CommodityUnitRelation, CommodityUnitAdmin)
+
